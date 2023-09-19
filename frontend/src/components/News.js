f0b0cb53e4508f98c863de2bb5cb7227c4cea4f2
Cassio Eduardo Gabriel Cordeiro <80807111>
2023-09-19 09:23:25 -0300

    add date picker

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -3,11 +3,10 @@ import { New } from '../components/New';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Badge from 'react-bootstrap/Badge';
 
-export const News = ({ papel }) => {
+export const News = ({ papel, data }) => {
     const [noticias, setNoticias] = useState([]);
 
     useEffect(() => {
-        const data = new Date().toLocaleDateString('en-CA');
         fetch(`http://localhost:8080?papel=${papel}&data=${data}`, { headers: { 'cache-control': 'no-cache', } })
             .then(data =>
                 data.json()
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { News } from '../components/News';
+import React, { useState } from "react";
+import { News } from "../components/News";
 
 export const Home = () => {
-    const papeis = ['xpml', 'visc','bcff', 'mxrf', 'irdm', 'hglg', 'xplg', 'alup', 'itsa', 'taee', 'pssa', 'klbn', 'vbbr'];
+  const papeis = [
+    "xpml",
+    "visc",
+    "bcff",
+    "mxrf",
+    "irdm",
+    "hglg",
+    "xplg",
+    "alup",
+    "itsa",
+    "taee",
+    "pssa",
+    "klbn",
+    "vbbr",
+    "csan",
+    "vale",
+  ];
+  const [date, setDate] = useState('');
 
-    return (
-        <div>
-            {papeis.map(papel => <News papel={papel} key={papel}/>) }
-        </div>
-    );
+  const handleDateChange = (e) => {
+    const value = e.target.value;
+    //const data = new Date(value).toLocaleDateString("en-CA");
+    setDate(value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input id="date" type="date" onChange={handleDateChange} />
+        {date}
+      </div>
+      <div>
+        {papeis.map((papel) => (
+          <News papel={papel} key={papel} data={date} />
+        ))}
+      </div>
+    </div>
+  );
 };
