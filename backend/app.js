43c52eb379fe59d24425bbd6e0d92fec2834fe86
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2024-07-04 22:20:09 -0300

    fix request

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
+const https = require('https');
 
 const PORT = process.env.PORT || "8080";
 const app = express();
@@ -14,22 +15,24 @@ app.get("/", (req, res) => {
     method: 'get',
     maxBodyLength: Infinity,
     url: `https://sistemasweb.b3.com.br/PlantaoNoticias/Noticias/ListarTitulosNoticias?agencia=18&palavra=${req.query.papel}&dataInicial=${data}&dataFinal=${data}`,
-    headers: {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-      'Accept-Language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es;q=0.6',
-      'Cache-Control': 'max-age=0',
-      'Connection': 'keep-alive',
-      'Cookie': 'TS01871345=016e3b076f18257e9b4c4a3e17b835b554465a12443cdacd8b3a60b3ca77aff0504dcb90c46bd3a3b43563f825c2b371fa283620f2; TS0134a800=016e3b076f18257e9b4c4a3e17b835b554465a12443cdacd8b3a60b3ca77aff0504dcb90c46bd3a3b43563f825c2b371fa283620f2; BIGipServerpool_sistemasweb.b3.com.br_443_WAF=4114163722.64288.0000; dtCookie=v_4_srv_27_sn_4C3CB9D440F180CD8942BCD6080D061F_perc_100000_ol_0_mul_1_app-3A6c080359c6da1713_1_app-3A2fa0c7805985f6bf_1_rcs-3Acss_0',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1',
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-      'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Linux"'
-    }
+    headers: { 
+      'Accept': '*/*', 
+      'Accept-Language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es;q=0.6', 
+      'Connection': 'keep-alive', 
+      'Referer': 'https://sistemasweb.b3.com.br/PlantaoNoticias/Noticias/Index?agencia=18', 
+      'Sec-Fetch-Dest': 'empty', 
+      'Sec-Fetch-Mode': 'cors', 
+      'Sec-Fetch-Site': 'same-origin', 
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 
+      'X-Requested-With': 'XMLHttpRequest', 
+      'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"', 
+      'sec-ch-ua-mobile': '?0', 
+      'sec-ch-ua-platform': '"Linux"', 
+      'Cookie': 'TS0134a800=016e3b076f25eebd844952580d55e81857a18a6b363fb18af143996c31d1ac0618ddc70334a5d8d15c26f4cde697dac6744560b52d; dtCookie=v_4_srv_24_sn_B04DDE7CA36EE7E7378A598CC1690EC5_perc_100000_ol_0_mul_1_app-3Afd69ce40c52bd20e_1_rcs-3Acss_0; BIGipServerpool_sistemasweb.b3.com.br_443_WAF=4114163722.64288.0000; TS01871345=016e3b076f25eebd844952580d55e81857a18a6b363fb18af143996c31d1ac0618ddc70334a5d8d15c26f4cde697dac6744560b52d'
+    },
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
   };
 
   axios.request(config)
